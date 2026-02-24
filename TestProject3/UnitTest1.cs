7cf98c921ba264d7ba472671edb90eaf7010d12a
Limolix <andrejnagirnak395@gmail.com>
2026-02-24 12:14:06 +0800

    add file tests

---
+++ TestProject3/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using task3;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject3.Tests
+{
+    [TestClass()]
+    public class Tests
+    {
+        [TestMethod()]
+        public void Test()
+        {
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual();
+        }
+    }
+}
--- task3/Program.cs
+++ task3/Program.cs
@@ -1,7 +1,24 @@
 ﻿namespace task3
 {
 
+    public class Logic // класс где будем хранить логику
+    {
+        public static int СountIdenticalLetters(string s)
+        {
+            char p = ' ';
+            int count = 0;
 
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                if (s[i] == s[i + 1] && s[i] != p && s[i + 1] != p)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+    }
 
 
 
@@ -13,18 +30,7 @@
             Console.WriteLine("Введите строку");
             string s = Console.ReadLine();
 
-
-            char p = ' ';
-            int count = 0;
-
-            for (int i = 0; i < s.Length - 1; i++)
-            {
-                if (s[i] == s[i + 1] && s[i] != p && s[i + 1] != p)
-                {
-                    count++;
-                }
-            }
-            Console.WriteLine(count);
+            Console.WriteLine(Logic.СountIdenticalLetters(s));
         }
     }
 
