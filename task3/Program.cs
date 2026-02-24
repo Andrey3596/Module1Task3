4f56f2148fed3b3f1dc74a35ff4aefc169ae9b16
Limolix <andrejnagirnak395@gmail.com>
2026-02-24 11:56:43 +0800

    add logic

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,15 @@
 ﻿Console.WriteLine("Введите строку");
 string s = Console.ReadLine();
+
+
+char p = ' ';
+int count = 0;
+
+for (int i = 0; i < s.Length - 1; i++)
+{
+    if (s[i] == s[i + 1] && s[i] != p && s[i + 1] != p)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
