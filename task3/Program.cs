32c24d31f966bafd5a00755e54bf5414a7daade7
Limolix <andrejnagirnak395@gmail.com>
2026-02-24 11:59:38 +0800

    add dec

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,15 +1,32 @@
-﻿Console.WriteLine("Введите строку");
-string s = Console.ReadLine();
-
-
-char p = ' ';
-int count = 0;
-
-for (int i = 0; i < s.Length - 1; i++)
+﻿namespace task3
 {
-    if (s[i] == s[i + 1] && s[i] != p && s[i + 1] != p)
+
+
+
+
+
+
+    class Program
     {
-        count++;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string s = Console.ReadLine();
+
+
+            char p = ' ';
+            int count = 0;
+
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                if (s[i] == s[i + 1] && s[i] != p && s[i + 1] != p)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
     }
+
+
 }
-Console.WriteLine(count);
